c0c2283da2c80baaad0f891483ad4382992d20d3
Jane Jeon <JaneJeon9719@gmail.com>
2018-03-19 03:30:22 -0700

    git fixed

--- access_token.example.php
+++ access_token.example.php
@@ -1,5 +1,3 @@
 <?php
-$username = 'YOUR_GITLAB_USERNAME_HERE';
-
 # tokens can be generated at https://gitlab.com/profile/personal_access_tokens
-const access_token = 'YOUR_GITLAB_ACCESS_TOKEN_HERE';
+return 'YOUR_GITLAB_ACCESS_TOKEN_HERE';
--- offline.php
+++ offline.php
@@ -1,4 +1,16 @@
 <?php
-// makes every GitLab repository private
+# makes every GitLab repository private
 
 $access_token = require 'access_token.php';
+
+function make_gitlab_request(string $access_token, string $uri, string $method = 'GET'): string {
+	return shell_exec("curl --header 'PRIVATE-TOKEN: $access_token' -X $method https://gitlab.com/api/v4$uri");
+}
+
+foreach (json_decode(make_gitlab_request($access_token, '/projects'), true) as $project)
+	if ($project['visibility'] != 'private')
+		# unfortunately, due to GitLab's fucky API, you only get '403 Forbidden' when trying to change visibility
+		# of *your* project (from somewhere else, e.g. GitHub) that *you* imported into *your* account
+		make_gitlab_request($access_token, '/projects/'.$project['id'].'?visibility=private', 'PUT');
+
+echo 'done!';
